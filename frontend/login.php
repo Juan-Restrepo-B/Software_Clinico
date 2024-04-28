7e1075911272e61164f1463caeb8a3ebf6bf8141
Juan-Restrepo-B <juanprestrepob@gmail.com>
2024-04-27 23:46:27 -0500

    cambios en el login

--- frontend/login.php
+++ frontend/login.php
@@ -2,7 +2,6 @@
 require '../backend/bd/Conexion.php';
 include '../backend/php/login.php';
 
-
 $settings = $connect->prepare("SELECT nomem, foto FROM settings");
 $settings->execute();
 $setting = $settings->fetch(PDO::FETCH_ASSOC);
@@ -11,6 +10,8 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 echo 'Hello, World!';
+
+phpinfo();
 ?>
 <!DOCTYPE html>
 <html lang="es">
